72d5f30418fbc2c619c68c155353a2f3f75268b7
msxtracool <larissasanntos@hotmail.com>
2025-06-23 20:28:18 +0200

    updates

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,9 +1,16 @@
+
+import { Link } from "react-router-dom";
+
 const About = () => {
   return (
     <>
       <h2>About This Project</h2>
-      <p >This is a simple e-commerce admin panel, created as a mini-project during a web development bootcamp. It's designed for basic store management.</p>
-      <button>Back</button>
+      <p>
+        This is a simple e-commerce admin panel, created as a mini-project during a web development bootcamp. It's designed for basic store management.
+      </p>
+      <Link to="/" className="button">
+        Back
+      </Link>
     </>
   );
 };
--- src/Pages/ProfilePage.jsx
+++ src/Pages/ProfilePage.jsx
@@ -1,5 +1,49 @@
-const ProfilePage = () => {
-  return <h2>Profile Page</h2>;
-};
+import { Link } from "react-router-dom";
+
+function ProfilePage() {
+  const userProfile = {
+    image: "https://t3.ftcdn.net/jpg/03/46/83/96/360_F_346839683_6nAPzbhpSkIpb8pmAwufkC7c5eD7wYws.jpg",
+    name: "Joshua Danielson ",
+    email: "Joshua Danielson@example.com",
+  };
+
+  return (
+    <div>
+      <h1>User Page</h1>
+      <div>
+        {userProfile && (
+          <>
+            <img
+              src={userProfile.image}
+              alt="profile-photo"
+                style={{
+                  width: "80px",
+                  height: "80px",
+                  borderRadius: "100%",
+                  objectFit: "cover",
+                  border: "2px  #d1d5db"
+                  }}
+            />
+            <h1 style={{fontSize:"20px"}}>
+              {userProfile.name}
+            </h1>
+
+            <div>
+              <p>
+                <strong>Email:</strong> {userProfile.email}
+              </p>
+            </div>
+          </>
+        )}
+
+        {/* Back button */}        
+       <Link to="/" className="button">
+        Back
+      </Link>
+        
+      </div>
+    </div>
+  );
+}
 
 export default ProfilePage;
